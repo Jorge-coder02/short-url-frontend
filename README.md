28d28356ba8d421f47da9e2a334ae9118fc9de35
Jorge-coder02 <jorgitenis@hotmail.es>
2025-02-26 18:37:58 +0100

    Subidos los archivos del Front

---
+++ README.md
@@ -0,0 +1,8 @@
+# Funcionamiento
+
+Acortador de URLs que recibe una URL. La URL es enviada con petición _POST_
+al backend (un servidor _separado del Front_), donde se valida, se _genera un ID_ único
+con nanoid y, posteriormente, se guardan el ID y la Url original en la bbdd de MongoDB.
+La base de datos consta de esos mismos 2 campos principales: _originalUrl y shortUrl_.
+La _respuesta_ que envía la bbdd al front es del ID corto generado, y se permite al usuario
+copiar el nuevo enlace o navegar directamente haciendo click en él
---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const URL_dominio = "http://localhost:3001";
+  const [urlInput, setUrlInput] = useState("");
+  const [respUrl, setRespUrl] = useState("");
+
+  const handleClick = async () => {
+    console.log("Enviando: ", urlInput);
+
+    // Enviar url al backend
+    const params = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Necesario para enviar datos JSON
+      },
+      body: JSON.stringify({ originalUrl: urlInput }), // Convertir la URL en JSON
+    };
+
+    try {
+      const resp = await fetch(URL_dominio, params);
+      if (resp.ok) {
+        const data = await resp.json();
+        setRespUrl(data.shortId); // Guardar la URL acortada en el estado
+        console.log("Respuesta servidor: ", data.shortId);
+      } else {
+        console.error("Error al generar la URL corta");
+      }
+    } catch (error) {
+      console.error("Error al hacer la solicitud", error);
+    }
+  };
+  return (
+    <div className="flex items-center justify-center pb-40 bg-slate-800 min-h-screen text-white">
+      <div className="flex flex-col gap-8 place-items-center bg-slate-700 px-20 py-10 rounded-xl">
+        <h1 className="text-4xl">ShortenURL by Jorge</h1>
+        <input
+          onChange={(e) => setUrlInput(e.target.value)}
+          type="text"
+          className="input px-20 text-center bg-slate-600 placeholder-white truncate"
+          placeholder="Inserta URL"
+        />
+        <button onClick={handleClick} className="btn px-20">
+          Generar URL
+        </button>
+        {respUrl && (
+          <p>
+            Tu url generada:{" "}
+            <a
+              href={`${URL_dominio}/${respUrl}`}
+            >{`${URL_dominio}/${respUrl}`}</a>{" "}
+            <button>Copiar</button>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
